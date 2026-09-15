e1ad1b199a66eca9ef45ef5667b1c9fd348dbc51
vovakonukh <v.konukh@gmail.com>
2026-09-15 22:06:28 +0300

    marquiz code

--- includes/header.php
+++ includes/header.php
@@ -54,6 +54,29 @@ _tmr.push({id: "3324398", type: "pageView", start: (new Date()).getTime(), pid: 
 </script>
 <!-- Roistat Counter End -->
 
+<!-- Marquiz script start -->
+<script>
+(function(w, d, s, o){
+  var j = d.createElement(s); j.async = true; j.src = '//script.marquiz.ru/v2.js';j.onload = function() {
+    if (document.readyState !== 'loading') Marquiz.init(o);
+    else document.addEventListener("DOMContentLoaded", function() {
+      Marquiz.init(o);
+    });
+  };
+  d.head.insertBefore(j, d.head.firstElementChild);
+})(window, document, 'script', {
+    host: '//quiz.marquiz.ru',
+    region: 'ru',
+    id: '67347643f5091100264023cb',
+    autoOpen: false,
+    autoOpenFreq: 'once',
+    openOnExit: false,
+    disableOnMobile: false
+  }
+);
+</script>
+<!-- Marquiz script end -->
+
 
 <section class="header">
     <div class="header_info">
